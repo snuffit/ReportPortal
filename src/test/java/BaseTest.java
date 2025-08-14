6f4262cbada52c0b1e78de2b2031adb05b595417
bludich <sadpufikot@gmail.com>
2025-08-14 18:24:29 +0300

    add BaseTest and PropertyReader with config.properties

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,48 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import utils.PropertyReader;
+
+import java.time.Duration;
+
+import static utils.AllureUtils.takeScreenshot;
+
+public class BaseTest {
+
+    WebDriver driver;
+    String login = System.getProperty("login", PropertyReader.getProperty("login"));
+    String password = System.getProperty("password", PropertyReader.getProperty("password"));
+
+    @Parameters
+    @BeforeMethod(alwaysRun = true, description = "Открытие браузера")
+    public void setup(@Optional("chrome") String browser) {
+        if (browser.equals("chrome")) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless");
+            driver = new ChromeDriver(options);
+        } else if (browser.equals("firefox")) {
+            FirefoxOptions options = new FirefoxOptions();
+            options.addArguments("--headless");
+            driver = new FirefoxDriver(options);
+        }
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod(alwaysRun = true, description = "Закрытие браузера")
+    public void tearDown(ITestResult result) {
+        if (ITestResult.FAILURE == result.getStatus()) {
+            takeScreenshot(driver);
+        }
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
